8e52303a542337e4040b7087b6f4f67cc4ac2b8b
Eider Muñoz <eider.munoz@asmetsalud.com>
2021-04-17 23:52:16 -0500

    Commit inicial

---
+++ README.md
@@ -0,0 +1,1 @@
+# back_apis_evertec
---
+++ src/main/java/com/example/back_apis/BackApisApplication.java
@@ -0,0 +1,13 @@
+package com.example.back_apis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackApisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackApisApplication.class, args);
+    }
+
+}
